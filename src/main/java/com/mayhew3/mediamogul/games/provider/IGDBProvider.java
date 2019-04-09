9f7f7134afa44e60ed3fa6e740fa59e8b1ab31b6
mayhew3 <commander.boffo@gmail.com>
2019-04-09 14:15:17 -0700

    Revert "Revert "MM-650: Resolve conflicts""
    This reverts commit 34bf5374c6ea407b4d9d57e040287b79893868d6.

--- src/main/java/com/mayhew3/mediamogul/games/provider/IGDBProvider.java
+++ src/main/java/com/mayhew3/mediamogul/games/provider/IGDBProvider.java
@@ -1,10 +1,13 @@
 package com.mayhew3.mediamogul.games.provider;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 public interface IGDBProvider {
 
   JSONArray findGameMatches(String gameTitle);
 
   JSONArray getUpdatedInfo(Integer igdb_id);
+
+  JSONObject getCoverInfo(Integer igdb_cover_id);
 }
