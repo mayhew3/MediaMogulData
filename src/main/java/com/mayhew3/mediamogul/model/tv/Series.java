9e5d3e0f5e3b3b5b37f3366f726b5fb15789a987
Mayhew Seavey <commander.boffo@gmail.com>
2019-11-24 16:38:08 -0800

    Suppress warnings

--- src/main/java/com/mayhew3/mediamogul/model/tv/Series.java
+++ src/main/java/com/mayhew3/mediamogul/model/tv/Series.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@SuppressWarnings({"WeakerAccess", "unused"})
 public class Series extends RetireableDataObject implements Comparable<Series> {
 
   /* Foreign Keys */
