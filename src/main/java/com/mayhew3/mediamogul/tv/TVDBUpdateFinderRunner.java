43db09d79b43c5f6b91032f4159a3e142bcac2fa
mayhew3 <commander.boffo@gmail.com>
2019-11-30 23:52:31 -0500

    MM-993: Add manual runner for TVDB Update Finder

--- src/main/java/com/mayhew3/mediamogul/tv/TVDBUpdateFinderRunner.java
+++ src/main/java/com/mayhew3/mediamogul/tv/TVDBUpdateFinderRunner.java
@@ -1,10 +1,35 @@
 package com.mayhew3.mediamogul.tv;
 
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mayhew3.mediamogul.ArgumentChecker;
+import com.mayhew3.mediamogul.ExternalServiceHandler;
+import com.mayhew3.mediamogul.ExternalServiceType;
+import com.mayhew3.mediamogul.db.ConnectionDetails;
+import com.mayhew3.mediamogul.exception.MissingEnvException;
+import com.mayhew3.mediamogul.tv.provider.TVDBJWTProviderImpl;
+import com.mayhew3.mediamogul.xml.JSONReaderImpl;
+import com.mayhew3.postgresobject.db.PostgresConnectionFactory;
+import com.mayhew3.postgresobject.db.SQLConnection;
+import org.apache.http.auth.AuthenticationException;
+
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+
 public class TVDBUpdateFinderRunner {
 
+  public static void main(String... args) throws URISyntaxException, SQLException, MissingEnvException, UnirestException, AuthenticationException {
+    ArgumentChecker argumentChecker = new ArgumentChecker(args);
 
+    ConnectionDetails connectionDetails = ConnectionDetails.getConnectionDetails(argumentChecker);
 
-  public static void main(String[] args) {
+    String dbUrl = connectionDetails.getDbUrl();
+    SQLConnection connection = PostgresConnectionFactory.initiateDBConnect(dbUrl);
+    ExternalServiceHandler tvdbServiceHandler = new ExternalServiceHandler(connection, ExternalServiceType.TVDB);
 
+    TVDBJWTProviderImpl tvdbJWTProvider = new TVDBJWTProviderImpl(tvdbServiceHandler);
+    JSONReaderImpl jsonReader = new JSONReaderImpl();
+
+    TVDBUpdateFinder tvdbUpdateFinder = new TVDBUpdateFinder(connection, tvdbJWTProvider, jsonReader);
+    tvdbUpdateFinder.runUpdate();
   }
 }
