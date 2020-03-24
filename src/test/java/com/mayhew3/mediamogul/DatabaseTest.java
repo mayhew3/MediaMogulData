351c94f1908051db5da0561ecf29e86b043155ac
mayhew3 <commander.boffo@gmail.com>
2020-03-24 00:40:00 -0700

    MM-1008: Temporarily create all the views manually in DatabaseTest. Want to move this to PostgresObject at some point.

--- src/test/java/com/mayhew3/mediamogul/DatabaseTest.java
+++ src/test/java/com/mayhew3/mediamogul/DatabaseTest.java
@@ -18,11 +18,66 @@ public abstract class DatabaseTest {
 
   private static Logger logger = LogManager.getLogger(DatabaseTest.class);
 
+  private void deleteValidView() throws SQLException {
+    String sql = "DROP VIEW IF EXISTS valid_episode";
+    connection.prepareAndExecuteStatementUpdate(sql);
+  }
+
+  private void deleteRegularView() throws SQLException {
+    String sql = "DROP VIEW IF EXISTS regular_episode";
+    connection.prepareAndExecuteStatementUpdate(sql);
+  }
+
+  private void deleteMatchingView() throws SQLException {
+    String sql = "DROP VIEW IF EXISTS matched_series";
+    connection.prepareAndExecuteStatementUpdate(sql);
+  }
+
+  private void deleteViews() throws SQLException {
+    deleteMatchingView();
+    deleteRegularView();
+    deleteValidView();
+  }
+
+  private void createValidView() throws SQLException {
+    String sql = "CREATE VIEW valid_episode " +
+        "AS SELECT * " +
+        "FROM episode " +
+        "WHERE retired = 0 " +
+        "AND tvdb_approval = 'approved' ";
+    connection.prepareAndExecuteStatementUpdate(sql);
+  }
+
+  private void createRegularView() throws SQLException {
+    String sql = "CREATE VIEW regular_episode " +
+        "AS SELECT * " +
+        "FROM valid_episode " +
+        "WHERE season <> 0 ";
+    connection.prepareAndExecuteStatementUpdate(sql);
+  }
+
+  private void createMatchingView() throws SQLException {
+    String sql = "CREATE VIEW matched_series " +
+        "AS SELECT * " +
+        "FROM series " +
+        "WHERE retired = 0 " +
+        "AND tvdb_match_status = 'Match Completed' ";
+    connection.prepareAndExecuteStatementUpdate(sql);
+  }
+
+  private void createViews() throws SQLException {
+    createValidView();
+    createRegularView();
+    createMatchingView();
+  }
+
   @Before
   public void setUp() throws URISyntaxException, SQLException, MissingEnvException {
     logger.info("Setting up test DB...");
     connection = PostgresConnectionFactory.getSqlConnection(PostgresConnectionFactory.TEST);
+    deleteViews();
     new DatabaseRecreator(connection).recreateDatabase(MediaMogulSchema.schema);
+    createViews();
     logger.info("DB re-created.");
   }
 
