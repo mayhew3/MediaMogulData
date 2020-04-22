8a6a220bb91f263e5971fc7086389f9914c7fa4d
mayhew3 <commander.boffo@gmail.com>
2020-04-21 17:45:45 -0700

    MM-1060: Add igdb_platform_id

--- src/main/java/com/mayhew3/mediamogul/model/games/GamePlatform.java
+++ src/main/java/com/mayhew3/mediamogul/model/games/GamePlatform.java
@@ -8,8 +8,9 @@ import com.mayhew3.postgresobject.dataobject.Nullability;
 public class GamePlatform extends DataObject {
 
   public FieldValueString fullName = registerStringField("full_name", Nullability.NOT_NULL);
-  public FieldValueString shortName = registerStringField("short_name", Nullability.NOT_NULL);
-  public FieldValueString igdbName = registerStringField("igdb_name", Nullability.NOT_NULL);
+  public FieldValueString shortName = registerStringField("short_name", Nullability.NULLABLE);
+  public FieldValueString igdbPlatformId = registerStringField("igdb_platform_id", Nullability.NULLABLE);
+  public FieldValueString igdbName = registerStringField("igdb_name", Nullability.NULLABLE);
   public FieldValueInteger parentPlatformID = registerIntegerField("parent_id", Nullability.NULLABLE);
 
   @Override
