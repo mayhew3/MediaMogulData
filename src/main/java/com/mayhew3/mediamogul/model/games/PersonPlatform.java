cfa3b2ff39e37a01b49612fd683ddb2eaaa08b4f
mayhew3 <commander.boffo@gmail.com>
2020-05-03 11:44:21 -0700

    MM-1061: Add PersonPlatform entity

---
+++ src/main/java/com/mayhew3/mediamogul/model/games/PersonPlatform.java
@@ -0,0 +1,29 @@
+package com.mayhew3.mediamogul.model.games;
+
+import com.mayhew3.mediamogul.model.Person;
+import com.mayhew3.postgresobject.dataobject.*;
+
+public class PersonPlatform extends DataObject {
+
+  public FieldValueForeignKey personID = registerForeignKey(new Person(), Nullability.NOT_NULL);
+  public FieldValueForeignKey gamePlatform = registerForeignKey(new GamePlatform(), Nullability.NOT_NULL);
+
+  public FieldValueString platformName = registerStringField("platform_name", Nullability.NOT_NULL);
+  public FieldValueInteger rank = registerIntegerField("rank", Nullability.NOT_NULL);
+
+  public PersonPlatform() {
+    super();
+    addUniqueConstraint(personID, gamePlatform, rank);
+  }
+
+  @Override
+  public String getTableName() {
+    return "person_platform";
+  }
+
+  @Override
+  public String toString() {
+    return "GamePlatform ID " + gamePlatform.getValue() + " for Person ID " + personID.getValue();
+  }
+
+}
