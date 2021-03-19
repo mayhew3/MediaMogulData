202881de5aca897fa0b54f50cef4169f64a52ded
Mayhew Seavey <commander.boffo@gmail.com>
2021-03-19 00:44:43 -0700

    FB-287: Update postgres-object, add v13 local env

--- src/main/java/com/mayhew3/mediamogul/db/DatabaseEnvironments.java
+++ src/main/java/com/mayhew3/mediamogul/db/DatabaseEnvironments.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class DatabaseEnvironments {
   public static Map<String, DatabaseEnvironment> environments = Map.ofEntries(
       new AbstractMap.SimpleEntry<>("local", new LocalDatabaseEnvironment("local", "tv", 11)),
+      new AbstractMap.SimpleEntry<>("local_13", new LocalDatabaseEnvironment("local_13", "tv", 13)),
       new AbstractMap.SimpleEntry<>("test", new LocalDatabaseEnvironment("test", "tv_copy", 11)),
       new AbstractMap.SimpleEntry<>("e2e", new LocalDatabaseEnvironment("e2e", "tv_e2e", 11)),
       new AbstractMap.SimpleEntry<>("heroku", new HerokuDatabaseEnvironment("heroku", "postgresURL_heroku", 11, "media-mogul")),
