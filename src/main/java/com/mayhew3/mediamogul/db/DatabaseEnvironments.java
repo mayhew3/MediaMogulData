4afb3ed7ba7d512844a0047fa78da408318000f2
Mayhew Seavey <commander.boffo@gmail.com>
2021-03-19 15:57:53 -0700

    FB-287: Remove last usage of other thing

--- src/main/java/com/mayhew3/mediamogul/db/DatabaseEnvironments.java
+++ src/main/java/com/mayhew3/mediamogul/db/DatabaseEnvironments.java
@@ -11,14 +11,14 @@ public class DatabaseEnvironments {
   public static Map<String, DatabaseEnvironment> environments = new HashMap<>();
 
   static {
-    addLocal("local", "tv", 11);
-    addLocal("local_13", "tv", 13);
-    addLocal("test", "tv_copy", 11);
-    addLocal("e2e", "tv_e2e", 11);
-    addHeroku("heroku", "postgresURL_heroku", 11, "media-mogul");
-    addHeroku("heroku-staging", "postgresURL_heroku_staging", 11, "media-mogul-staging");
+    addLocal("local", "tv", 13);
+    addLocal("test", "tv_copy", 13);
+    addLocal("e2e", "tv_e2e", 13);
+    addHeroku("heroku", "postgresURL_mediamogul", 11, "media-mogul");
+    addHeroku("heroku-staging", "postgresURL_mediamogul_staging", 11, "media-mogul-staging");
   }
 
+  @SuppressWarnings("SameParameterValue")
   private static void addLocal(String environmentName, String databaseName, Integer pgVersion) {
     Integer port = 5432 - 9 + pgVersion;
     LocalDatabaseEnvironment local = new LocalDatabaseEnvironment(environmentName, databaseName, port, pgVersion);
