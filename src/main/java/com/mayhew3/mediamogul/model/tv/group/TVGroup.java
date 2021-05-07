6a170582092c2f4ec57d23c13e9ec781b742e270
Mayhew Seavey <commander.boffo@gmail.com>
2021-05-06 17:40:14 -0700

    MM-1105: Update PG-Obj, use strict type

--- src/main/java/com/mayhew3/mediamogul/model/tv/group/TVGroup.java
+++ src/main/java/com/mayhew3/mediamogul/model/tv/group/TVGroup.java
@@ -1,16 +1,13 @@
 package com.mayhew3.mediamogul.model.tv.group;
 
-import com.mayhew3.postgresobject.dataobject.FieldValue;
-import com.mayhew3.postgresobject.dataobject.FieldValueString;
-import com.mayhew3.postgresobject.dataobject.Nullability;
-import com.mayhew3.postgresobject.dataobject.RetireableDataObject;
+import com.mayhew3.postgresobject.dataobject.*;
 
 import java.math.BigDecimal;
 
 public class TVGroup extends RetireableDataObject {
 
   public FieldValueString name = registerStringField("name", Nullability.NULLABLE);
-  public FieldValue<BigDecimal> minWeight = registerBigDecimalField("min_weight", Nullability.NOT_NULL).defaultValue(BigDecimal.valueOf(0.6));
+  public FieldValueBigDecimal minWeight = registerBigDecimalField("min_weight", Nullability.NOT_NULL).defaultValue(BigDecimal.valueOf(0.6));
 
   public TVGroup() {
     super();
