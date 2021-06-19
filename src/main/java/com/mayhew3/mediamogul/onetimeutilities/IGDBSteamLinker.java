773435a472aecc9fb798044af4d67f642be5fcd9
Mayhew Seavey <commander.boffo@gmail.com>
2021-06-19 15:06:40 -0700

    MM-1180: Utility works

--- src/main/java/com/mayhew3/mediamogul/onetimeutilities/IGDBSteamLinker.java
+++ src/main/java/com/mayhew3/mediamogul/onetimeutilities/IGDBSteamLinker.java
@@ -4,6 +4,7 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import com.mayhew3.mediamogul.db.DatabaseEnvironments;
 import com.mayhew3.mediamogul.games.provider.IGDBProvider;
 import com.mayhew3.mediamogul.games.provider.IGDBProviderImpl;
+import com.mayhew3.mediamogul.model.games.Game;
 import com.mayhew3.mediamogul.xml.JSONReader;
 import com.mayhew3.mediamogul.xml.JSONReaderImpl;
 import com.mayhew3.postgresobject.ArgumentChecker;
@@ -13,8 +14,11 @@ import com.mayhew3.postgresobject.db.SQLConnection;
 import com.mayhew3.postgresobject.exception.MissingEnvException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.net.URISyntaxException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class IGDBSteamLinker {
@@ -31,7 +35,7 @@ public class IGDBSteamLinker {
     this.jsonReader = jsonReader;
   }
 
-  public static void main(String... args) throws SQLException, MissingEnvException, URISyntaxException, UnirestException {
+  public static void main(String... args) throws SQLException, MissingEnvException, URISyntaxException, UnirestException, InterruptedException {
     ArgumentChecker argumentChecker = new ArgumentChecker(args);
 
     DatabaseEnvironment environment = DatabaseEnvironments.getEnvironmentForDBArgument(argumentChecker);
@@ -41,10 +45,70 @@ public class IGDBSteamLinker {
     igdbSteamLinker.runUpdate();
   }
 
-  private void runUpdate() {
+  private void runUpdate() throws SQLException, InterruptedException {
     String sql = "SELECT * " +
         "FROM game " +
-        "WHERE steamid IS NULL ";
+        "WHERE steamid IS NULL " +
+        "AND igdb_id IS NOT NULL ";
 
+    ResultSet resultSet = connection.prepareAndExecuteStatementFetch(sql);
+    while (resultSet.next()) {
+      Game game = new Game();
+      game.initializeFromDBObject(resultSet);
+
+      Integer igdbID = game.igdb_id.getValue();
+
+      JSONArray updatedInfoArray = igdbProvider.getUpdatedInfo(igdbID);
+      if (updatedInfoArray.length() != 1) {
+        logger.debug("Expected exactly one match for game with igdb_id: " + game.igdb_id.getValue() + ", " +
+            "but there are " + updatedInfoArray.length());
+      } else {
+        logger.debug(" - Found IGDB data matching existing ID. Updating.");
+        JSONObject updatedInfo = updatedInfoArray.getJSONObject(0);
+        if (updatedInfo.has("status") && 401 == updatedInfo.getInt("status")) {
+          logger.error("Error code returned by IGDB.");
+          logger.error("Title: " + updatedInfo.getString("title"));
+          logger.error("Cause: " + updatedInfo.getString("cause"));
+          logger.error("Details: " + updatedInfo.getString("details"));
+          throw new IllegalStateException("Failure fetching from IGDB.");
+        }
+        Integer steamID = findSteamID(updatedInfo);
+        String title = game.title.getValue();
+        if (steamID != null) {
+          logger.debug("Found Steam ID " + steamID + " for game '" + title + "'");
+          game.steamID.changeValue(steamID);
+          try {
+            game.commit(connection);
+          } catch (SQLException e) {
+            logger.debug("Failed to update game '" + title + "'");
+          }
+        } else {
+          logger.debug("No Steam ID for game '" + title + "'");
+        }
+      }
+
+      //noinspection BusyWait
+      Thread.sleep(250);
+
+    }
+  }
+
+  private Integer findSteamID(JSONObject updatedInfo) {
+    JSONArray websites = jsonReader.getArrayWithKey(updatedInfo, "websites");
+    for (Object websiteObj : websites) {
+      JSONObject website = (JSONObject) websiteObj;
+      String url = jsonReader.getStringWithKey(website, "url");
+      if (url.startsWith("https://store.steampowered.com")) {
+        String[] split = url.split("/");
+        String finalPiece = split[split.length - 1];
+        try {
+          return Integer.parseInt(finalPiece);
+        } catch (NumberFormatException e) {
+          logger.debug("Illegal ID: " + finalPiece);
+          return null;
+        }
+      }
+    }
+    return null;
   }
 }
